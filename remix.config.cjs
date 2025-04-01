e5494cb07e7ed7a3011c995dd3534f289166ba9e
Onur Mutlu <onurmutlu@gmail.com>
2025-04-01 23:56:08 +0300

    vercel.json: update vercel.json to use the new build command

--- remix.config.cjs
+++ remix.config.cjs
@@ -5,13 +5,5 @@ module.exports = {
   ignoredRouteFiles: ["**/.*"],
   publicPath: "/build/",
   assetsBuildDirectory: "public/build",
-  serverBuildPath: "build/index.js",
-  tailwind: true,
-  future: {
-    v3_fetcherPersist: true,
-    v3_lazyRouteDiscovery: true,
-    v3_relativeSplatPath: true,
-    v3_singleFetch: true,
-    v3_throwAbortReason: true
-  }
+  serverBuildPath: "build/index.js"
 };
